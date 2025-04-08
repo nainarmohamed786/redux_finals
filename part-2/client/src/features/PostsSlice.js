403f5d455b91457c9513dfe02a1de24f725b4447
nainarmohamed786 <nainarmohammed226@gmail.com>
2025-04-08 12:51:36 +0530

    ui

---
+++ part-2/client/src/features/PostsSlice.js
@@ -0,0 +1,17 @@
+import {createSlice} from '@reduxjs/toolkit';
+
+
+const initialState=[
+    {
+        id:"0",
+        title:"Hello hii",
+        content:"if ther jfdhcvrf fvrv"
+    },
+    {
+        id:"1",
+        title:"hii hello",
+        content:"jhvgdchbvc hfcfejvgckwfe cvbfewvcjhwfev"
+    }
+];
+
+const PostsSl
